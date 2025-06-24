084fae6bc02a6480d8f0acc5242dd2b920a63312
irfan-ubpk23 <if23.muhamadfauzi@mhs.ubpkarawang.ac.id>
2025-06-24 20:53:46 +0700

    memfix kategori api
    Signed-off-by: irfan-ubpk23 <if23.muhamadfauzi@mhs.ubpkarawang.ac.id>

--- app/Http/Controllers/API/KategoriController.php
+++ app/Http/Controllers/API/KategoriController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use App\Services\Kategori;
+use App\Services\KategoriService;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
